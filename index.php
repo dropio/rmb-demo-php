bb048c7a9245993311798cf659a17e59f889dc47
Eric Skiff <eric@dropio.com>
2010-06-21 19:02:00 -0400

    added email and permalink functionality

--- index.php
+++ index.php
@@ -38,12 +38,16 @@ $alltypes = array("image", "movie", "audio", "document", "other", "note");
 //Fetch all assets in the drop into a global $assets variable
 $assets = array();
 $assetCount = array();
-while ( ($assetsIn = $drop->getAssets($page)) && $assetsIn->getCount()) {
-	foreach ($assetsIn as $assetIn){
-		$assets[] = $assetIn;
-		$assetCount[$assetIn->type]++;
+if($_REQUEST['viewmode'] == 'permalink'){//it's a permalink, just get the requested asset
+	$assets[] = $drop->getAsset($_REQUEST['assetid']);
+}else{
+	while ( ($assetsIn = $drop->getAssets($page)) && $assetsIn->getCount()) {
+		foreach ($assetsIn as $assetIn){
+			$assets[] = $assetIn;
+			$assetCount[$assetIn->type]++;
+		}
+		$page++;
 	}
-	$page++;
 }
 
 //////////////////////
@@ -56,6 +60,17 @@ if($_REQUEST["action"] == "delete" && $_REQUEST["assetid"]){
 			$a->delete();
 			//also remove that asset from the local array
 			unset($assets[$counter]);
+		}
+		$counter++;
+	}
+}else if($_REQUEST["action"] == "emailthis" && $_REQUEST["assetid"]){
+ 	//iterate through assets
+	$counter = 0;
+ 	foreach($assets as $a){
+		if($a->{$a->primary_key} == $_REQUEST["assetid"]){
+			$mail_sent = SendAssetEmail($a, $_REQUEST["emailaddresses"]);
+			//echo 'sending asset #' . $counter;
+			break;
 		}
 		$counter++;
 	}
@@ -110,12 +125,16 @@ if($_REQUEST["action"] == "delete" && $_REQUEST["assetid"]){
 </head>
 <body>
 
-<?php /* 
+<?php 
+if($mail_sent){
+	echo "<h2>Mail sent</h2>";
+}
+/* 
 ####################################################################################### 
 ###Media rendering mode################################################################ 
 #######################################################################################  */ ?>
 <?php 
-if (empty($_REQUEST["viewmode"]) || $_REQUEST["viewmode"] == 'media') 
+if (empty($_REQUEST["viewmode"]) || $_REQUEST["viewmode"] == 'media' || $_REQUEST["viewmode"] == 'permalink') 
 { ?>
 <style type="text/css">
 	body{background:url('images/fancybg.png') #dbdbdb repeat-x;}
@@ -126,9 +145,15 @@ if (empty($_REQUEST["viewmode"]) || $_REQUEST["viewmode"] == 'media')
 <script src='osflv/AC_RunActiveContent.js' language='javascript'></script>
 
 <div id="assets">
-	<h2 style="font-size:24px">Viewing the items in the drop: <?php echo  $dropname ?> </h2>
-	<h4>Switch to
-	<a href="<?php echo $_SERVER['PHP_SELF'] . '?viewmode=detailed&dropname='.$dropname; ?>">detailed view</a> to see more information about these assets, or view them <a href="<?php echo $_SERVER['PHP_SELF'] . '?viewmode=sorted&dropname='.$dropname; ?>">sorted by type</a></h4>
+	<?php if ($_REQUEST['viewmode'] == 'permalink') { ?>
+			<h2 style="font-size:24px">Viewing the asset <?php echo  $_REQUEST['assetid'] ?> the drop: <?php echo  $dropname ?> </h2>
+			<h4><a href="<?php echo $_SERVER['PHP_SELF'] . '?viewmode=media&dropname='.$dropname; ?>">&laquo;View all assets in this drop</a></h4>
+	<?php }else{?>
+			<h2 style="font-size:24px">Viewing the items in the drop: <?php echo  $dropname ?> </h2>
+			<h4>Switch to
+			<a href="<?php echo $_SERVER['PHP_SELF'] . '?viewmode=detailed&dropname='.$dropname; ?>">detailed view</a> to see more information about these assets, or view them <a href="<?php echo $_SERVER['PHP_SELF'] . '?viewmode=sorted&dropname='.$dropname; ?>">sorted by type</a></h4>
+	<?php } ?>
+
 		<br />
 		<table><tr><th width="250">Title &amp; Description</th><th width="400">Preview</th><th width="50">Links</th></tr> 
 		<?php 
@@ -334,6 +359,106 @@ if (empty($_REQUEST["viewmode"]) || $_REQUEST["viewmode"] == 'media')
 <?php } ?>
 </body></html>
 <?php
+function SendAssetEmail($a, $emails){
+	global $drop, $assets, $API_KEY, $dropname, $enabled_cdns, $assetCount;
+	//define the receiver of the email
+	$to = $emails;
+	//define the subject of the email
+	$subject = 'You\'ve been sent a file using the Drop.io RMB PHP demo'; 
+	//define the headers we want passed. Note that they are separated with \r\n
+	$headers = "From: eric@dropio.com\r\nReply-To: eric@dropio.com\r\n";
+	//add boundary string and mime type specification
+	// Always set content-type when sending HTML email
+	$headers .= "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+	//define the body of the message.
+	$message = '<h2>You\'ve been sent the file '. $a->{$a->primary_key} .'</h2>
+	<p>You can check this file out here:<br /><a href="http://' . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"] . '?viewmode=permalink&assetid='.$a->{$a->primary_key}.'&dropname='. $dropname .'">http://' . $_SERVER["HTTP_HOST"] . $_SERVER["PHP_SELF"] . '?viewmode=permalink&assetid='.$a->{$a->primary_key}.'&dropname='. $dropname .'</a></p> 
+	';
+	
+	if($a->type == 'image'){
+		$message .= '<h3>Preview:</h3>';
+		$message .= GetAssetPreview($a);
+	}
+
+	//send the email
+	//echo 'sending to ' . $to;
+	$mail_sent = @mail( $to, $subject, $message, $headers );
+	return($mail_sent);
+}
+function GetAssetPreview($a){
+	if ($a->type == "image"){
+		foreach ($a->roles as $name=>$r) { 
+			if ($r["name"] == "large_thumbnail"){ 
+				if ($r["locations"][0]["status"] == "complete"){
+					$preview = "<img style='width:240px;' src=\"". $r["locations"][0]["file_url"] . "\" alt='".htmlspecialchars($a->name)."'>";
+					$preview .= "<br />Original width = " . $dimensions['width'] . ", height = " .$dimensions['height'];
+				}
+			}
+			if ($r["name"] == "original_content"){
+				$dimensions['width'] = $r["width"];
+				$dimensions['height'] = $r["height"];
+			} 
+		}
+	} elseif ($a->type == "audio"){
+		if ($a->roles[0]["locations"][0]["status"] == "complete"){
+			#$preview = '<embed type="application/x-shockwave-flash" src="http://www.google.com/reader/ui/3247397568-audio-player.swf?audioUrl='
+			#$preview .= CGI::escape origfile
+			#$preview .= '" width="400" height="27" allowscriptaccess="never" quality="best" bgcolor="#ffffff" wmode="window" flashvars="playerMode=embedded" />'
+
+			$preview = '<object type="application/x-shockwave-flash" data="wpaudio/player.swf" id="';
+			$preview .= 'ap-' + $a->name;
+			$preview .= '" height="24" width="290">
+			<param name="movie" value="wpaudio/player.swf">
+			<param name="FlashVars" value="playerID=';
+			$preview .= 'ap-' + $a->name;
+			$preview .= '&soundFile=';
+			$preview .= urlencode($origfile);
+			$preview .= '">
+			<param name="quality" value="high">
+			<param name="menu" value="false">
+			<param name="wmode" value="transparent">
+			</object>';
+			#$preview .= $a->inspect
+		}
+	}elseif ($a->type == "movie"){
+		$flv = '';
+		foreach ($a->roles as $name=>$r) {  
+			if ($r["name"] == "web_preview") {
+				if ($r["locations"][0]["status"] == "complete"){
+					$flv = $r["locations"][0]["file_url"];
+					$preview = "<object width='400' height='325' id='flvPlayer'>
+					  <param name='allowFullScreen' value='true'>
+					  <param name='movie' value='osflv/OSplayer.swf?movie=";
+					$preview .= urlencode($flv);
+					$preview .= "&btncolor=0x333333&accentcolor=0x31b8e9&txtcolor=0xdddddd&volume=30&previewimage=&autoload=off&vTitle=&showTitle=yes'>
+					  <embed src='osflv/OSplayer.swf?movie=";
+					$preview .=  urlencode($flv);
+					$preview .= "&btncolor=0x333333&accentcolor=0x31b8e9&txtcolor=0xdddddd&volume=30&previewimage=&autoload=off&vTitle=&showTitle=yes' width='400' height='325' allowFullScreen='true' type='application/x-shockwave-flash'>
+					 </object>";
+				}
+			}
+		}
+	}elseif ($a->type == "document"){
+		foreach ($a->roles as $name=>$r) {  
+			if ($r["name"] == "web_preview"){
+				if ($r["locations"][0]["status"] == "complete"){
+					$docurl = $r["locations"][0]["file_url"];
+					$preview .= "<iframe style='width:600px;height:802px' frameborder='0' src=\"http://docs.google.com/viewer?embedded=true&url=";
+					$preview .= urlencode($docurl);
+					$preview .= "\"></iframe>";
+				}
+			}
+		}
+	}elseif ($a->type == "note"){
+			$preview = $a->roles[0]["contents"];
+			#echo print_r($a);
+	}else{
+		$preview = "<a href='". $origfile . "'><img src='images/downloaddisk.png' style='border:none' alt='download'/></a>";
+		#$preview = h($a->inspect)
+	}
+	return $preview;
+}
 function GetAssetsByType($type = array("image", "movie", "audio", "document", "other", "note")){
 	global $drop, $assets, $API_KEY, $dropname, $enabled_cdns, $assetCount;
 	$page = 1;
@@ -360,82 +485,19 @@ function GetAssetsByType($type = array("image", "movie", "audio", "document", "o
 				</td>
 				<td>
 					<?php 
-					$preview = '';
-					if ($a->type == "image"){
-						foreach ($a->roles as $name=>$r) { 
-							if ($r["name"] == "large_thumbnail"){ 
-								if ($r["locations"][0]["status"] == "complete"){
-									$preview = "<img style='width:240px;' src=\"". $r["locations"][0]["file_url"] . "\" alt='".htmlspecialchars($a->name)."'>";
-									$preview .= "<br />Original width = " . $dimensions['width'] . ", height = " .$dimensions['height'];
-								}
-							}
-							if ($r["name"] == "original_content"){
-								$dimensions['width'] = $r["width"];
-								$dimensions['height'] = $r["height"];
-							} 
-						}
-					} elseif ($a->type == "audio"){
-						if ($a->roles[0]["locations"][0]["status"] == "complete"){
-							#$preview = '<embed type="application/x-shockwave-flash" src="http://www.google.com/reader/ui/3247397568-audio-player.swf?audioUrl='
-							#$preview .= CGI::escape origfile
-							#$preview .= '" width="400" height="27" allowscriptaccess="never" quality="best" bgcolor="#ffffff" wmode="window" flashvars="playerMode=embedded" />'
-				
-							$preview = '<object type="application/x-shockwave-flash" data="wpaudio/player.swf" id="';
-							$preview .= 'ap-' + $a->name;
-							$preview .= '" height="24" width="290">
-							<param name="movie" value="wpaudio/player.swf">
-							<param name="FlashVars" value="playerID=';
-							$preview .= 'ap-' + $a->name;
-							$preview .= '&soundFile=';
-							$preview .= urlencode($origfile);
-							$preview .= '">
-							<param name="quality" value="high">
-							<param name="menu" value="false">
-							<param name="wmode" value="transparent">
-							</object>';
-							#$preview .= $a->inspect
-						}
-					}elseif ($a->type == "movie"){
-						$flv = '';
-						foreach ($a->roles as $name=>$r) {  
-							if ($r["name"] == "web_preview") {
-								if ($r["locations"][0]["status"] == "complete"){
-									$flv = $r["locations"][0]["file_url"];
-									$preview = "<object width='400' height='325' id='flvPlayer'>
-									  <param name='allowFullScreen' value='true'>
-									  <param name='movie' value='osflv/OSplayer.swf?movie=";
-									$preview .= urlencode($flv);
-									$preview .= "&btncolor=0x333333&accentcolor=0x31b8e9&txtcolor=0xdddddd&volume=30&previewimage=&autoload=off&vTitle=&showTitle=yes'>
-									  <embed src='osflv/OSplayer.swf?movie=";
-									$preview .=  urlencode($flv);
-									$preview .= "&btncolor=0x333333&accentcolor=0x31b8e9&txtcolor=0xdddddd&volume=30&previewimage=&autoload=off&vTitle=&showTitle=yes' width='400' height='325' allowFullScreen='true' type='application/x-shockwave-flash'>
-									 </object>";
-								}
-							}
-						}
-					}elseif ($a->type == "document"){
-						foreach ($a->roles as $name=>$r) {  
-							if ($r["name"] == "web_preview"){
-								if ($r["locations"][0]["status"] == "complete"){
-									$docurl = $r["locations"][0]["file_url"];
-									$preview .= "<iframe style='width:600px;height:802px' frameborder='0' src=\"http://docs.google.com/viewer?embedded=true&url=";
-									$preview .= urlencode($docurl);
-									$preview .= "\"></iframe>";
-								}
-							}
-						}
-					}elseif ($a->type == "note"){
-							$preview = $a->roles[0]["contents"];
-							#echo print_r($a);
-					}else{
-						$preview = "<a href='". $origfile . "'><img src='images/downloaddisk.png' style='border:none' alt='download'/></a>";
-						#$preview = h($a->inspect)
-					} 
+					$preview = GetAssetPreview($a);
 					echo $preview; 
 					?>
+						<div id="emailthis-<?php echo $a->{$a->primary_key}; ?>" style="display:none;"><form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post"> 
+	<input type="text" name="emailaddresses" /><br />
+	<input type="hidden" name="assetid" value="<?php echo $a->{$a->primary_key};  ?>" />
+	<input type="hidden" name="dropname" value="<?php echo $dropname; ?>" />
+	<input type="hidden" name='viewmode' value='<?php echo $_REQUEST["viewmode"]; ?>' />
+	<input type="hidden" name="action" value="emailthis" />
+	<input type="submit" value="Send this asset" /></form></div>
 					</td>
 					<td><?php if ($a->type != "note") { ?><a href="<?php echo $origfile; ?>">Download File</a><?php } ?>
-						<hr />	<a href="<?php echo $_SERVER['PHP_SELF'] . '?dropname='.$dropname.'&viewmode='.$_REQUEST['viewmode'].'&action=delete&assetid='.$a->{$a->primary_key} ?>" >delete asset</a>
+						<hr />	<a href="<?php echo $_SERVER['PHP_SELF'] . '?dropname='.$dropname.'&viewmode='.$_REQUEST['viewmode'].'&action=delete&assetid='.$a->{$a->primary_key} ?>" >delete asset</a><hr />	<a href="#" onclick="$('#emailthis-<?php echo $a->{$a->primary_key}; ?>').toggle(400);return false;" >Email asset</a>
 					
 					</td>
 				</tr>
