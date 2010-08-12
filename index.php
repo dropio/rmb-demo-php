013212244786d1a9c726984ea88b9f96be47cae6
Sheraz <sheraz@mail.ethershow.com>
2010-08-12 19:42:53 +0000

    addded location optional param to getUploadifyForm()

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+    <head>
+        <title>Drop.io RMB PHP Demo Applications</title>
+        <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css"/>
+    </head>
+    <body>
+        <div id="container">
+        <h1>Drop.io RMB Demo Applications for PHP</h1>
+
+        <p>Thank you for downloading the demo applications. Please explore the 
+        source code and documentation to learn more about making your
+        applications integration with the Drop.io Rich Media Backbone (RMB).</p>
+
+
+        <h2><a href="0-simple_demo/">Simple Demo</a></h2>
+        <p>The Simple Demo is a simple application that shows how easy it is to
+        send and retrieve files from Drop.io using our API.</p>
+
+        <h2><a href="1-advanced_demo/">Advanced Demo</a></h2>
+        <p>The Advanced demo builds on the simple demo by including a database,
+        Ajax, and rich media tools for managing your content.</p>
+        </div>
+
+
+    </body>
+</html>
