e6a8a81aec5fe4ce561b64b9eaa09ab759591fb8
Sheraz <sheraz@mail.ethershow.com>
2010-08-13 22:34:21 +0000

    used movie thumbnail rather than wed_preview

--- 1-advanced_demo/templates/list-movie.php
+++ 1-advanced_demo/templates/list-movie.php
@@ -2,7 +2,7 @@ List | Detailed
 <hr/>
 <?php foreach ($assets as $a):   ?>
      <div class="thumb">
-         <img src="<?php echo get_thumb($a,'web_preview') ?>" alt=""/>
+         <img src="<?php echo get_thumb($a,'thumbnail') ?>" alt=""/>
          <b>Name:</b> <?php echo $a['name'] ?><br/>
          <b>Description:</b> <?php //echo $a['description'] ?>
     </div>
