3d45f8576c7313232c040165d1a518c242e88ac7
Sheraz <sheraz@mail.ethershow.com>
2010-08-13 22:31:42 +0000

    added partial html fragment for audio display

---
+++ 1-advanced_demo/templates/list-audio.php
@@ -0,0 +1,8 @@
+List | Detailed
+<hr/>
+<?php foreach ($assets as $a):   ?>
+     <div class="thumb">
+         <b>Name:</b> <?php echo $a['name'] ?><br/>
+         <b>Description:</b> <?php //echo $a['description'] ?>
+    </div>
+<?php endforeach ?>
