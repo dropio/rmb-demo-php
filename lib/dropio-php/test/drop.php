71716f639fe8d31f09108bb05351c901d9294b60
Sheraz <sheraz@mail.ethershow.com>
2010-08-18 12:59:55 +0000

    stubbed out more test cases

---
+++ lib/dropio-php/test/drop.php
@@ -0,0 +1,74 @@
+<?php
+
+include_once 'config.php';
+
+include_once 'PHPUnit/Framework.php';
+include_once '../Dropio/Drop.php';
+
+
+class Dropio_DropTest extends PHPUnit_Framework_TestCase
+{
+    const PAGINATION = 31; // Test pagination
+
+    protected $api   = null;
+    protected $drop  = null;
+    protected $drops = array(); //array of drops
+
+    public function setup()
+    {
+      $this->drop = new Dropio_Drop(Fixture::API_KEY);
+    }
+
+    public function tearDown()
+    {
+
+    }
+
+    public function testListAssets()
+    {
+      $this->drop->setName('yourmomgoestocollege')->load();
+    }
+
+    public function testListAssetsWithPagination()
+    {
+      $this->assertTrue(FALSE);
+    }
+
+    public function testAddAsset()
+    {
+
+      $this->assertTrue(FALSE);
+    }
+
+    public function deleteAsset()
+    {
+      $this->assertTrue(FALSE);
+
+    }
+
+    public function testCreateADrop()
+    {
+      $this->assertTrue(FALSE);
+
+    }
+
+    public function testDeleteADrop()
+    {
+      $this->assertTrue(FALSE);
+
+    }
+
+    public function testUpdateADrop()
+    {
+      $this->assertTrue(FALSE);
+
+    }
+
+    public function testEmptyADrop()
+    {
+      $this->assertTrue(FALSE);
+
+    }
+
+
+}
