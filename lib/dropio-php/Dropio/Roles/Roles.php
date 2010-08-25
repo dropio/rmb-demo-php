49e0560f0c49153ec9846a616feaa534320321d7
Sheraz <sheraz@mail.ethershow.com>
2010-08-25 21:17:49 +0000

    constructor now takes values, fixed arrays working with [location]

--- lib/dropio-php/Dropio/Roles/Roles.php
+++ lib/dropio-php/Dropio/Roles/Roles.php
@@ -11,23 +11,22 @@ Abstract Class Role Implements RoleType
 {
     
     const DEF_LOCATION  = 'DropioS3';
-    private $values    = array();
+    private $values     = array();
 
-    public function __construct()
+    public function __construct($values=array())
     {
+        $this->values = $values;
 
     }
 
     public function getName()     { return $this->values['name']; }
     public function getFileSize() { return $this->values['filesize']; }
-    public function getName()     { return $this->values['name']; }
 
     public function getLocation($name=self::DEF_LOCATION)
     {
       foreach($this->values['locations'] as $loc)
       {
-        if($loc['location']['name'] == $name)
-            return $loc['location'];
+        if($loc['name'] == $name) { return $loc; }
       }
 
       return false;
@@ -36,12 +35,13 @@ Abstract Class Role Implements RoleType
     public function getStatus($location=self::DEF_LOCATION)
     {
         $loc = $this->getLocation($location);
-        return (isset($loc['location']['status']) ? $log['location']['status'] : false;
+        return (isset($loc['status'])) ? $log['status'] : false;
     }
 
     public function getFileUrl($location=self::DEF_LOCATION)
     {
         $loc = $this->getLocation($location);
-        return (isset($loc['location']['file_url']) ? $log['location']['file_url'] : false;
+        return (isset($loc['file_url'])) ? $loc['file_url'] : false;
     }
+
 }
