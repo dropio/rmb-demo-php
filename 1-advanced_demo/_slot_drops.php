90a61feb1f989bd2b55c75a02ab720448b940336
Sheraz <sheraz@mail.ethershow.com>
2010-08-25 21:22:54 +0000

    syntax chages to API

--- 1-advanced_demo/_slot_drops.php
+++ 1-advanced_demo/_slot_drops.php
@@ -1,10 +1,15 @@
-<?php $droplist = Dropio_Api::getInstance($API_KEY)->getDrops(); ?>
+<?php 
+
+$drops = DB::getInstance()->query("SELECT drop_name from asset GROUP BY drop_name ORDER BY drop_name")->fetchAll();
+
+?>
+
 <!-- Begin drops slot -->
 <div id="drops">
 <h4>Drops</h4>
 <ul>
-<?php foreach ($droplist['drops'] as $d): ?>
-<li><a href="assets-view.php?drop_name=<?php echo $d['name'] ?>"><?php echo $d['name'] ?></a></li>
+<?php foreach ($drops as $d): ?>
+    <li><a href="assets-view.php?drop_name=<?php echo $d['drop_name'] ?>"><?php echo $d['drop_name'] ?></a></li>
 <?php endforeach ?>
 </ul>
 </div>
