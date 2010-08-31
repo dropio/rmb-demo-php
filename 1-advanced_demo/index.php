52e23846b7a9c9d6015c9c719fe20a01fda95164
Sheraz Sharif <sheraz@under-new-management.com>
2010-08-31 13:12:00 -0400

    homepage now uses jquery, ajaxy modal popups for managing drops, and now displays messages taken from session variables

--- 1-advanced_demo/index.php
+++ 1-advanced_demo/index.php
@@ -2,17 +2,36 @@
 
 include_once '_bootstrap.php';
 
+# Get all drops from the database
 $drops = DB::getInstance()->
             query("SELECT * FROM `drop`")->
             fetchAll();
 
-var_dump($drops);
 ?>
 
 <html>
     <head>
         <title></title>
         <link rel="stylesheet" type="text/css" href="../css/main.css"/>
+
+        <!-- Load jQuery -->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+
+        <!-- Load Fancybox -->
+        <script type="text/javascript" src="../utils/fancybox/jquery.fancybox-1.3.1.pack.js"></script>
+        <link rel="stylesheet" type="text/css" href="../utils/fancybox/jquery.fancybox-1.3.1.css"/>
+
+        <!-- Initialze Fancybox -->
+        <script type="text/javascript" language="javascript">
+            $(document).ready(function() {
+                $(".fancyform").fancybox({
+                 'type' : 'iframe',
+                 'onClosed' : function(){
+                     location.reload();
+                 }
+                });
+            });
+        </script>
     </head>
     <body>
         <div id="container">
@@ -26,8 +45,15 @@ var_dump($drops);
             <li>Pingback Url: <?php echo "{$docroot}pingback.php"?></li>
         </ul>
         <hr/>
-        <p>Database actions: <a href="drop-import_drop.php">Import a drop</a> | Delete a drop </p>
+        <p>Database actions: <a class="fancyform" href="drop-create_drop.php">Create drop</a> | <a class="fancyform" href="drop-import_drop.php">Import a drop</a></p>
         <h4>Drops currently in your database</h4>
+
+        <?php if($message): ?>
+        <div id="message">
+          <?php echo $message ?>
+        </div>
+        <?php endif ?>
+
         <?php if(count($drops) > 0): ?>
         <ul>
             <?php foreach ($drops as $d): ?>
@@ -36,12 +62,7 @@ var_dump($drops);
         </ul>
         <?php else: ?>
         
-            <p style="color: red">There are no drops. Create one now.
-                <form action="drop-create_drop.php" method="post">
-                    Drop Name: <input type="text" name="drop_name"/>
-                    <input type="submit"/>
-                </form>
-            </p>
+            <p style="color: red">There are no drops. <a class="fancyform" href="drop-create_drop.php">Create one now.</a></p>
         <?php endif ?>
         
         <hr/>
