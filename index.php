d3ffbee866140cd79f03313993f1848d90b4a008
Eric Skiff <eric@dropio.com>
2010-09-02 10:18:23 -0400

    Copy changes

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <html>
     <head>
-        <title>Drop.io RMB PHP Demo Applications</title>
+        <title>Drop.io RMB Demo Applications for PHP</title>
         <link rel="stylesheet" type="text/css" media="screen" href="/css/main.css"/>
     </head>
     <body>
         <div id="container">
         <h1>Drop.io RMB Demo Applications for PHP</h1>
 
-        <p>Thank you for downloading the demo applications. Please explore the 
-        source code and documentation to learn more about making your
-        applications integration with the Drop.io Rich Media Backbone (RMB).</p>
-
-
+        <p>These examples demonstrate how you can use the Drop.io Rich Media Backbone along
+	 	with our PHP helper library to
+		handle file uploads, conversion, storage, and delivery in your applications.</p>
+		<p><a href="http://backbone.drop.io" target="_blank">Learn more about the Rich Media Backbone</a></p>
+		
         <h2><a href="0-simple_demo/">Simple Demo</a></h2>
-        <p>The Simple Demo is a simple application that shows how easy it is to
-        send and retrieve files from Drop.io using our API.</p>
+        <p>The Simple demo provides examples of each API call using our PHP helper library.</p>
 
         <h2><a href="1-advanced_demo/">Advanced Demo</a></h2>
-        <p>The Advanced demo builds on the simple demo by including a database,
-        Ajax, and rich media tools for managing your content.</p>
+        <p>The Advanced demo presents a 
+		complete functional workflow for accepting user uploads, handling asynchronous conversion
+		pingbacks, and updating the client in realtime when files are converted using our XMPP stream.
+		This demo can also function as a scaffold that you can modify and build upon to create your own
+		applications.</p>
+		
+		<p>To run the advanced demo on your own server, you will a mysql database for the application to 
+				store information about assets, as well as a publicly accessible web address for accepting
+				pingback POSTs from the Rich Media Backbone when your conversions are complete.</p>
         </div>
 
 
