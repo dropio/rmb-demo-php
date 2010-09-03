8ff3b147abf9bcbbca31c9d16bceae8157680282
bryanwoods <bryan@dropio.com>
2010-09-03 14:41:31 -0400

    Adding explicit width and height to fancymovie fancybox

--- 1-advanced_demo/assets-view.php
+++ 1-advanced_demo/assets-view.php
@@ -73,7 +73,7 @@ $(document).ready(function() {
          location.reload();
      }
     });
-    
+
     // Handle the delete button
     $("#deletedrop").fancybox({
      'type' : 'iframe',
@@ -87,22 +87,24 @@ $(document).ready(function() {
     $(".fancyimage").fancybox({
       'type' : 'image'
     });
-    
+
     $('.fancydocument').each(function(){
       $(this).fancybox({
         'type' : 'iframe',
         'href' : 'http://docs.google.com/viewer?embedded=true&url=' + $(this).attr('href')
-      });    
+      });
     });
 
     $('.fancymovie').each(function(){
       $(this).fancybox({
-        'type' : 'iframe',
-        'href' : '<?php echo $docroot ?>/1-advanced_demo/_video_player.php?file=' + $(this).attr('href') + '&poster=' + $(this).attr('poster')
+        'type'      : 'iframe',
+        'width'     : 640,
+        'height'    : 480,
+        'href'      : '<?php echo $docroot ?>/1-advanced_demo/_video_player.php?file=' + $(this).attr('href') + '&poster=' + $(this).attr('poster')
       });
     });
-    
-    
+
+
 });
 
 </script>
@@ -113,18 +115,18 @@ $(document).ready(function() {
 
         <hr class="Solid"/>
           <?php include_once('_slot_drops.php'); ?>
-        
+
           <a href="<?php echo $docroot ?>/1-advanced_demo">Home</a> &gt; <?php echo $_GET['drop_name']?> &gt; <?php echo $type ?>
           <div style="float: right">
               <?php include_once('_slot_uploadify_form.php'); ?>
           </div>
 
           <hr class="Dotted"/>
-        
+
           <a id="deletedrop" href="drop-delete_drop.php?drop_name=<?php echo $_GET['drop_name'] ?>">Delete this drop</a>
-        
+
           <hr class="Dotted"/>
-        
+
           <a href="assets-view.php?drop_name=<?php echo $_GET['drop_name']?>&type=all">All</a> |
           <a href="assets-view.php?drop_name=<?php echo $_GET['drop_name']?>&type=image">Images</a> |
           <a href="assets-view.php?drop_name=<?php echo $_GET['drop_name']?>&type=movie">Movies</a> |
@@ -133,9 +135,9 @@ $(document).ready(function() {
           <a href="assets-view.php?drop_name=<?php echo $_GET['drop_name']?>&type=notes">Notes</a> |
           <a href="assets-view.php?drop_name=<?php echo $_GET['drop_name']?>&type=link">Links</a> |
           <a href="assets-view.php?drop_name=<?php echo $_GET['drop_name']?>&type=other">Other</a>
-                
+
             <hr class="Dotted"/>
-        
+
           <div id="content-container">
           <?php foreach ($arr as $k=>$v):   ?>
           <div class="container" id="<?php echo $k ?>-container">
