ca27c62bb3ca9e8c25ff8d4c03fd94e6a630beb0
bryanwoods <bryan@dropio.com>
2010-09-03 14:42:32 -0400

    Cleaning out some hard tabs that were inserted

--- 1-advanced_demo/_slot_uploadify_form.php
+++ 1-advanced_demo/_slot_uploadify_form.php
@@ -3,9 +3,9 @@
 <link rel="stylesheet" type="text/css" media="screen, projection" href="../utils/uploadify/uploadify.css" />
 
 <?php
-	$drop_name = $_GET['drop_name'];
-	$drop = Dropio_Drop::getInstance($API_KEY, $API_SECRET)->load($drop_name);
-	$chatPass = $drop->getChatPassword();
+  $drop_name = $_GET['drop_name'];
+  $drop = Dropio_Drop::getInstance($API_KEY, $API_SECRET)->load($drop_name);
+  $chatPass = $drop->getChatPassword();
 ?>
 
 <script type="text/javascript">// <![CDATA[
@@ -13,13 +13,13 @@
 $(document).ready(function() {
 
     var api = new DropioApiClient("<?php echo $API_KEY ?>","<?php echo $docroot ?>/DropioJSClientXDReceiver.html");
-	
+
     var dropCB = function(response, status){
         var chatPass = "<?php echo $chatPass ?>";
 
         DropioStreamer.start("<?php echo $_GET['drop_name'] ?>",chatPass,"<?php echo $docroot ?>/streamer_xdr.html");
         DropioStreamer.observe(DropioStreamer.ASSET_UPDATED,function(data){
-            
+
             // Shortcut to the objects we want
             var type = data.type;         // image, movie, document, etc
             var r = data.roles.role;
@@ -36,17 +36,17 @@ $(document).ready(function() {
             {
               case 'image' :
                 console.log('inside switch');
-                if (r.name == 'thumbnail') { 
+                if (r.name == 'thumbnail') {
                   console.log('r.name is thumbnail: ' + l.file_url);
-                  document.getElementById(data.name).innerHTML = '<img src="' + l.file_url + '"/>'; 
-                }
-                break;
-                
-              case 'movie'  :
-                if (r.name == 'thumbnail') { 
                   document.getElementById(data.name).innerHTML = '<img src="' + l.file_url + '"/>';
                 }
-                if (r.name == 'web_preview') { 
+                break;
+
+              case 'movie'  :
+                if (r.name == 'thumbnail') {
+                  document.getElementById(data.name).innerHTML = '<img src="' + l.file_url + '"/>';
+                }
+                if (r.name == 'web_preview') {
                   var myLink = document.getElementById(data.name);
                   myLink.setAttribute('href',l.file_url);
                   $(myLink).each(function(){
@@ -55,14 +55,14 @@ $(document).ready(function() {
                       'href' : '<?php echo $docroot ?>/1-advanced_demo/_video_player.php?file=' + $(this).attr('href') + '&poster=' + $(this).attr('poster')
                     });
                   });
-                  
+
                 }
-                if (r.name == 'large_thumbnail') { 
-                  document.getElementById(data.name).setAttribute('poster',l.file_url); 
+                if (r.name == 'large_thumbnail') {
+                  document.getElementById(data.name).setAttribute('poster',l.file_url);
                 }
-                
+
                 break;
-                
+
               case 'document' :
                 if (r.name == 'web_preview') {
                   var myLink = document.getElementById(data.name);
@@ -71,14 +71,14 @@ $(document).ready(function() {
                     $(this).fancybox({
                       'type' : 'iframe',
                       'href' : 'http://docs.google.com/viewer?embedded=true&url=' + $(this).attr('href')
-                    });    
+                    });
                   });
-                  
+
                 }
                 break;
-                
+
               case 'audio' :
-              
+
                 break;
             }
 
@@ -101,14 +101,14 @@ console.log(j);
         // Create the new element
         var newAsset = document.createElement('div');
         newAsset.setAttribute('class','thumb');
-        
+
         var myLink = document.createElement('a');
         myLink.setAttribute('class','fancy'+j.type);
         myLink.setAttribute('id',j.name);
 
         switch(j.type)
         {
-          case 'image' : 
+          case 'image' :
             myLink.setAttribute('href',j.roles[0].locations[0].file_url);
             $(myLink).fancybox({'type':'image'});
             break;
@@ -125,10 +125,10 @@ console.log(j);
 
         myLink.innerHTML = '<img src="images/spinner.gif" alt="Not ready yet"/>';
         newAsset.appendChild(myLink);
-        
+
         // Get the containing div. If it does not exist then create it
         typecont = document.getElementById(j.type+'-container');
-        
+
         if (typecont == null)
         {
           // Create the container
@@ -136,24 +136,24 @@ console.log(j);
           typecont.setAttribute('style','clear:both');
           typecont.setAttribute('id',j.type+'-container');
           typecont.innerHTML = '<h3>' + j.type + '</h3>';
-          
+
           document.getElementById('content-container').appendChild(typecont);
         }
         document.getElementById(j.type+'-container').appendChild(newAsset);
         return true;
     };
 <?php
-	$sigdata = '';
-	if(!empty($API_SECRET)){
-		$timestamp = time() + (60 * 60);
-		$paramsToSign = array();
-		$paramsToSign["version"] = "3.0";
-		$paramsToSign["timestamp"] = $timestamp;
-		$paramsToSign["signature_mode"] = "OPEN";
-		$paramsToSign["api_key"] = $API_KEY;
-		$signedParams = Dropio_Api::getInstance($API_KEY, $API_SECRET)->signRequest($paramsToSign);
-		$sigdata .= ',"timestamp":"'.$timestamp.'","signature_mode":"OPEN","signature":"'.$signedParams["signature"].'"';
-	}
+  $sigdata = '';
+  if(!empty($API_SECRET)){
+    $timestamp = time() + (60 * 60);
+    $paramsToSign = array();
+    $paramsToSign["version"] = "3.0";
+    $paramsToSign["timestamp"] = $timestamp;
+    $paramsToSign["signature_mode"] = "OPEN";
+    $paramsToSign["api_key"] = $API_KEY;
+    $signedParams = Dropio_Api::getInstance($API_KEY, $API_SECRET)->signRequest($paramsToSign);
+    $sigdata .= ',"timestamp":"'.$timestamp.'","signature_mode":"OPEN","signature":"'.$signedParams["signature"].'"';
+  }
 ?>
 $('#file').uploadify({
     'uploader'  : '../utils/uploadify/uploadify.swf',
