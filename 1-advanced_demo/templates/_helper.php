ee2862a13e061119a4cb4536d8eee1f76985e45a
Bryan Woods <bryan@dropio.com>
2010-09-08 11:18:30 -0400

    Load original content for PDFs in fancybox but web previews for any other type of document

--- 1-advanced_demo/templates/_helper.php
+++ 1-advanced_demo/templates/_helper.php
@@ -4,7 +4,7 @@ function get_file_url($a,$type)
     $vals = json_decode($a['values'],true);
 
     if ($vals == NULL || $vals==FALSE)
-        return 'images/spinner.gif'; 
+        return 'images/spinner.gif';
 
     # Dig down into that array
     foreach($vals['roles'] as $v)
@@ -31,17 +31,24 @@ function show_audio($a)
     <script type="text/javascript" language="javascript">AudioPlayer.embed("{$a['name']}", {soundFile: "$file_url"});</script>
     <b>Name:</b> {$a['name']}<br/>
     <b>Description:</b>
-    
+
 EOF;
     return $html;
 }
 
 /**
- *  Get an HTML snippit that shows an icon and links to a web-preview of a document
+ *  Get an HTML snippit that shows an icon and links to a document
  */
 function show_document($a)
 {
-    return '<a class="fancydocument" href="'. get_file_url($a,'web_preview').'"><img src="images/pdf_icon.jpg"/></a>' . substr($a['name'],0,15) . '<br/>';
+    if (substr($a['name'], -3, 3) == "pdf") {
+        # Document is a PDF. Link to original content.
+        $doclink = 'original_content';
+    } else {
+        # Document is not a PDF. Link to web preview.
+        $doclink = 'web_preview';
+    }
+    return '<a class="fancydocument" href="' . get_file_url($a, $doclink) . '"><img src="images/pdf_icon.jpg"/></a>' . substr($a['name'], 0, 15) . '<br/>';
 }
 
 function show_movie($a)
