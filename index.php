2283c161471241089985de4fcbc111b40606cc24
Bryan Woods <bryan@dropio.com>
2010-09-14 13:06:20 -0400

    Making some copy edits to home page

--- index.php
+++ index.php
@@ -8,23 +8,23 @@
         <h1>Drop.io RMB Demo Applications for PHP</h1>
 
         <p>These examples demonstrate how you can use the Drop.io Rich Media Backbone along
-	 	with our PHP helper library to
-		handle file uploads, conversion, storage, and delivery in your applications.</p>
-		<p><a href="http://backbone.drop.io" target="_blank">Learn more about the Rich Media Backbone</a></p>
-		
+     with our <a href="http://github.com/dropio/dropio-php" target="_blank">PHP helper library</a> to
+    handle file uploads, conversion, storage, and delivery in your applications.</p>
+    <p><a href="http://backbone.drop.io" target="_blank">Learn more about the Rich Media Backbone</a></p>
+
         <h2><a href="0-simple_demo/">Simple Demo</a></h2>
-        <p>The Simple demo provides examples of each API call using our PHP helper library.</p>
+        <p>The simple demo provides examples of each API call using our PHP helper library.</p>
 
         <h2><a href="1-advanced_demo/">Advanced Demo</a></h2>
-        <p>The Advanced demo presents a 
-		complete functional workflow for accepting user uploads, handling asynchronous conversion
-		pingbacks, and updating the client in realtime when files are converted using our XMPP stream.
-		This demo can also function as a scaffold that you can modify and build upon to create your own
-		applications.</p>
-		
-		<p>To run the advanced demo on your own server, you will a mysql database for the application to 
-				store information about assets, as well as a publicly accessible web address for accepting
-				pingback POSTs from the Rich Media Backbone when your conversions are complete.</p>
+        <p>The advanced demo presents a
+    complete functional workflow for accepting user uploads, handling asynchronous conversion
+    pingbacks, and updating the client in realtime when files are converted using our XMPP stream.
+    This demo can also function as a scaffold that you can modify and build upon to create your own
+    applications.</p>
+
+    <p>To run the advanced demo on your own server, you will need to create a MySQL database for the application to
+        store information about assets, as well as a publicly accessible web address for accepting
+        pingback POSTs from the Rich Media Backbone when your conversions are complete.</p>
         </div>
 
 
