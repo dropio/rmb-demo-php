3b1d318c337125642d11a4d98f30947277c800ff
Eric Skiff <eric@dropio.com>
2010-09-22 12:31:24 -0400

    handle unexpected pingback functions, an always log for now

--- 1-advanced_demo/pingback.php
+++ 1-advanced_demo/pingback.php
@@ -1,12 +1,15 @@
 <?php
 /**
- * This is the core file for pingbacks. It will determine the approriate
+ * This is the core file for pingbacks. It will determine the appropriate
  * action to take based on the ping coming back from drop.io.
  *
  */
 
 # Bootstrap the page for app-wide functions. This means loading configs, classes,
 # and sesison variables
+$handled_pingback_functions = array('ASSET_UPDATED');
+
+
 include_once '_bootstrap.php';
 
 # Load the user functions
@@ -15,12 +18,18 @@ include_once('user_funcs.php');
 $event = strtolower($_POST['event']);
 
 # Are we debugging this?
-if (DEBUG===true)
-{
-    dolog(implode(' ',array_keys($_POST))) ;
+//if (DEBUG===true)
+//{
+	dolog(implode(' ',array_keys($_REQUEST))) ;
     dolog($_POST['event']);
     dolog($_POST['asset']);
-}
+//}
 
-# Call the appripriate function for the kind of event pinging back to us
-call_user_func($event, $_POST['asset']);
+# Call the appropriate function for the kind of event pinging back to us
+if(in_array($event, $handled_pingback_functions)){
+	call_user_func($event, $_POST['asset']);
+}else{
+	//if (DEBUG === true){
+		dolog('Unhandled pingback call: ' . $event);
+	//}
+}
