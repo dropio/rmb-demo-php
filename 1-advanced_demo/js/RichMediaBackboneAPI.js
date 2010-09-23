70a800d06ce3bfb58465d1eccb15dab2ff0f12f8
Eric Skiff <eric@dropio.com>
2010-09-23 14:33:03 -0400

    Point streamer at staging

--- 1-advanced_demo/js/RichMediaBackboneAPI.js
+++ 1-advanced_demo/js/RichMediaBackboneAPI.js
@@ -10,9 +10,9 @@ http://www.opensource.org/licenses/mit-license.php
 DropioApiClient = (function(){
   
   var undefined,
-    HOST = "http://stage.drop.io/",
-    API_HOST = "http://stage-api.drop.io/",
-    UPLOAD_HOST = "http://stage-assets.drop.io/",
+    HOST = "http://stage-release.drop.io/",
+    API_HOST = "http://stage-release-api.drop.io/",
+    UPLOAD_HOST = "http://stage-release-assets.drop.io/",
     DEFAULT_XR_SRC = "/DropioJSClientXDReceiver.html",
     
     DROPS_PATH = API_HOST + "drops",
